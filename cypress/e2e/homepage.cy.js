80e143e325d31063366e17f7e4d373125e50aca4
nargesAhmadieh <nargesahmadieh999@gmail.com>
2024-02-07 22:10:32 +0330

    added changes

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,34 +1,30 @@
 describe("Homepage", () => {
   beforeEach(() => {
-    cy.intercept("GET", "http://localhost:9000/films", {
-      fixture: "films.json",
-    }).as("getFilms");
+    cy.intercept("GET", "http://localhost:9000/films").as("getFilms");
     cy.visit("/");
-    // cy.wait("@getFilms");
+    cy.wait("@getFilms");
   });
 
   it("displays films fetched from the API", () => {
-    cy.get(".homepage").should("be.visible");
-    cy.get(".homepage h1").should("contain", "فیلم ها");
-    cy.get(".homepage ul li").should("have.length", 6);
+    cy.get("._homepage_jlns0_1").should("be.visible");
+    cy.get("._homepage_jlns0_1 h1").should("contain", "فیلم ها");
+    cy.get("._homepage_jlns0_1 ul li").should("have.length", 6);
   });
 
   it("deletes a film when delete button is clicked", () => {
-    cy.get(".homepage ul li").first().as("firstFilm");
+    cy.get("._homepage_jlns0_1 ul li").first().as("firstFilm");
     cy.get("@firstFilm")
       .find("h2")
       .invoke("text")
       .then((title) => {
         cy.get("@firstFilm").find("button").click();
-        cy.get("@firstFilm").should("not.exist");
-        cy.get(".homepage ul li").should("not.contain", title);
+        cy.get("._homepage_jlns0_1 ul li").should("not.contain", title);
       });
   });
 
   it("persists film list in local storage after deletion", () => {
-    cy.get(".homepage ul li").first().as("firstFilm");
+    cy.get("._homepage_jlns0_1 ul li").first().as("firstFilm");
     cy.get("@firstFilm").find("button").click();
-    cy.reload();
-    cy.get(".homepage ul li").should("have.length", 5);
+    cy.get("._homepage_jlns0_1 ul li").should("have.length", 5);
   });
 });
